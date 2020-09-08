290cd7bd5109afb029f236fbe928d0a0b358a47d
Will Bashelor <wbashelor@gmail.com>
2020-09-08 17:41:27 -0400

    Delayed geoquiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Geonames from 'geonames.js';
 import { Point } from 'react-simple-maps';
 
@@ -11,23 +11,26 @@ const geonames = new Geonames({
   encoding: 'JSON',
 });
 
-const App = React.memo(function () {
+function App() {
   const [currentWiki, setCurrentWiki] = useState('');
+  useEffect(() => {
+    console.log('currentWiki', currentWiki);
+  }, [currentWiki]);
   async function findWikipedia(centroid: Point): Promise<string> {
     const wiki = await geonames.findNearbyWikipedia({
       lat: centroid[1],
       lng: centroid[0],
     });
-    // setCurrentWiki(wiki);
-    console.log('currentWiki', currentWiki);
+    setCurrentWiki(wiki.geonames[0].summary);
     return wiki;
   }
   return (
     <React.Fragment>
-      <Quiz />
+      <div style={{ color: '#fff' }}>{currentWiki}</div>
+      {/* <Quiz currentWiki={currentWiki} /> */}
       <CT findWikipedia={findWikipedia} />
     </React.Fragment>
   );
-});
+}
 
 export default App;
