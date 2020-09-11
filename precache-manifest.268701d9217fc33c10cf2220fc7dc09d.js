0a571fab3087fde660f3a33cf417adba6445117b
Will Bashelor <wbashelor@gmail.com>
2020-09-11 05:45:30 -0400

    Updates

---
+++ precache-manifest.268701d9217fc33c10cf2220fc7dc09d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5c1022ad10b16b44fd33ebae19f648",
+    "url": "/geo-quiz/index.html"
+  },
+  {
+    "revision": "a1c68b238fadf7b69e5b",
+    "url": "/geo-quiz/static/css/main.8f602a5f.chunk.css"
+  },
+  {
+    "revision": "92086dbc12493b88a394",
+    "url": "/geo-quiz/static/js/2.7ae2358b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/geo-quiz/static/js/2.7ae2358b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c68b238fadf7b69e5b",
+    "url": "/geo-quiz/static/js/main.7243e5be.chunk.js"
+  },
+  {
+    "revision": "12f9f178b673d50ecd3d",
+    "url": "/geo-quiz/static/js/runtime-main.2e77119e.js"
+  }
+]);
